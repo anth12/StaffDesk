1f3e1665163fcc48b1d3e0440d11b829bd001817
Anthony Halliday <anthony.halliday@fatmedia.co.uk>
2021-11-14 09:12:11 +0000

    Adding HR application layer Create/Update

---
+++ src/StaffDesk.Application/Department/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StaffDesk.Application.Contracts.Department;
+using StaffDesk.Infrastructure.Contracts;
+
+namespace StaffDesk.Application.Department
+{
+	internal class DepartmentService : IDepartmentService
+	{
+		private readonly IDataContext _dataContext;
+
+		public DepartmentService(IDataContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
+		
+		public Task<Domain.Department[]> GetAllAsync()
+		{
+			return _dataContext.Department.ToArrayAsync();
+		}
+	}
+}
---
+++ src/StaffDesk.Application/HumanResource/HumanResourceService.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using StaffDesk.Application.Contracts.HumanResource;
+using StaffDesk.Infrastructure.Contracts;
+
+namespace StaffDesk.Application.HumanResource
+{
+	internal class HumanResourceService : IHumanResourceService
+	{
+		private readonly IDataContext _dataContext;
+
+		public HumanResourceService(IDataContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
+
+		public async Task<Domain.HumanResource> GetAsync(int id)
+		{
+			var result = await _dataContext.HumanResource.FindAsync(id);
+
+			return result;
+		}
+
+		public async Task<Domain.HumanResource> CrateOrUpdateAsync(HumanResourceUpdate request)
+		{
+			Domain.HumanResource entity;
+			if (request.Id > 0)
+			{
+				entity = await _dataContext.HumanResource.FindAsync(request.Id);
+			}
+			else
+			{
+				entity = new Domain.HumanResource();
+				_dataContext.HumanResource.Add(entity);
+			}
+
+			await MapToDomainAsync(request, entity);
+
+			await _dataContext.SaveChangesAsync(CancellationToken.None);
+
+			return entity;
+		}
+
+		public async Task MapToDomainAsync(HumanResourceUpdate request, Domain.HumanResource entity)
+		{
+			entity.Id = request.Id;
+			entity.FirstName = request.FirstName;
+			entity.LastName = request.LastName;
+			entity.EmailAddress = request.EmailAddress;
+			entity.DateOfBirth = request.DateOfBirth;
+			entity.Status = request.Status;
+			entity.EmployeeNumber = request.EmployeeNumber;
+
+			var department = await _dataContext.Department.FindAsync(request.DepartmentId);
+			entity.Department = department;
+		}
+	}
+}
